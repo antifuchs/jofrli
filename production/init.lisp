3e8be185eb6b36e3bd5c5d2af47567bfba57081c
Andreas Fuchs <asf@boinkor.net>
2010-12-22 16:04:15 +0100

    Add swank and hunchentoot start invocations.

--- production/init.lisp
+++ production/init.lisp
@@ -8,4 +8,10 @@
 (load "/opt/lisp/ql/quicklisp/setup.lisp")
 (push (merge-pathnames #P"../" *load-truename*))
 
+(ql:quickload :swank)
 (ql:quickload :jofrli)
+
+(let ((swank::*loopback-interface* "10.0.9.1"))
+  (swank:create-server :port *swank-port* :dont-close t))
+
+(print (jofrli:run-server))
